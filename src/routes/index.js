bd2439ec5459ec69c2197d3b7b015cd41e14d4a2
kienhoangg04 <kienkic0403@gmail.com>
2023-05-06 22:05:15 +0700

    Course Detail Page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const newsRouter = require('./news');
+const coursesRouter = require('./courses');
 const siteRouter = require('./site');
 
 function route(app) {
     // News
     app.use('/news', newsRouter);
+    // Course
+    app.use('/courses', coursesRouter);
 
     // Home, Search,...
     app.use('/', siteRouter);
