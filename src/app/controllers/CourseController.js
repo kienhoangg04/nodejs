792f2963aa23a417421fd332c75f82f0f15a7fcf
kienhoangg04 <kienkic0403@gmail.com>
2023-05-07 14:14:50 +0700

    Create new Course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -12,6 +12,22 @@ class CourseController {
             })
             .catch(next);
     }
+
+    // [GET] /courses/create
+    create(req, res) {
+        res.render('courses/create');
+    }
+
+    // [POST] /courses/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://i.ytimg.com/vi/${formData.videoId}/hqdefault.jpg`;
+        const course = new Course(formData);
+        course
+            .save()
+            .then(() => res.redirect('/'))
+            .catch((error) => {});
+    }
 }
 
 module.exports = new CourseController();
